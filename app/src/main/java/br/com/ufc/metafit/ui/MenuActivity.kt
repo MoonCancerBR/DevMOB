35535fbd752efd9a1bfb27d5b39e9b009c4f0ecb
MoonCancerBR <fernandogamorim730@gmail.com>
2023-11-30 14:23:17 -0300

    Update menu contatos

--- app/src/main/java/br/com/ufc/metafit/ui/MenuActivity.kt
+++ app/src/main/java/br/com/ufc/metafit/ui/MenuActivity.kt
@@ -1,41 +1,86 @@
-package br.com.ufc.metafit.ui
+package br.com.ufc.metafit.ui;
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import androidx.cardview.widget.CardView
-import br.com.ufc.metafit.R
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import androidx.cardview.widget.CardView;
+import br.com.ufc.metafit.R;
 
-class MenuActivity : AppCompatActivity() {
+//class MenuActivity : AppCompatActivity() {
+//
+//    private lateinit var caixa: CardView
+//    private lateinit var admin: CardView
+//    private lateinit var ofertas: CardView
+//    private lateinit var condicoes: CardView
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_menu)
+//
+//        caixa = findViewById(R.id.idcard1)
+//        admin = findViewById(R.id.idcard2)
+//        ofertas = findViewById(R.id.idcard3)
+//        condicoes = findViewById(R.id.idcard4)
+//        caixa.setOnClickListener {
+//            startActivity(Intent(this, CaixaActivity::class.java))
+//        }
+//
+//        admin.setOnClickListener {
+//            startActivity(Intent(this, AdminActivity::class.java))
+//        }
+//
+//        ofertas.setOnClickListener {
+//            startActivity(Intent(this, OfertaActivity::class.java))
+//        }
+//
+//        condicoes.setOnClickListener {
+//            startActivity(Intent(this, RulerActivity::class.java))
+//        }
+//    }
+//}
+public class MenuActivity extends AppCompatActivity {
+    CardView caixa, admin, ofertas,condicoes;
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        caixa = (CardView) findViewById(R.id.idcard1);
+        admin = (CardView) findViewById(R.id.idcard2);
+        ofertas = (CardView) findViewById(R.id.idcard3);
+        condicoes = (CardView) findViewById(R.id.idcard4);
 
-    private lateinit var caixa: CardView
-    private lateinit var admin: CardView
-    private lateinit var ofertas: CardView
-    private lateinit var condicoes: CardView
+        getCaixa();
+        getAdmin();
+        getOferta();
+        getCondicoes();
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_menu)
-
-        caixa = findViewById(R.id.idcard1)
-        admin = findViewById(R.id.idcard2)
-        ofertas = findViewById(R.id.idcard3)
-        condicoes = findViewById(R.id.idcard4)
-
-        caixa.setOnClickListener {
-            startActivity(Intent(this, CaixaActivity::class.java))
-        }
-
-        admin.setOnClickListener {
-            startActivity(Intent(this, AdminActivity::class.java))
-        }
-
-        ofertas.setOnClickListener {
-            startActivity(Intent(this, OfertaActivity::class.java))
-        }
-
-        condicoes.setOnClickListener {
-            startActivity(Intent(this, RulerActivity::class.java))
-        }
     }
+    private void getCaixa(){
+        caixa.setOnClickListener((v)->{
+            Intent i = new Intent(MenuActivity.this, CaixaActivity.class);
+            startActivity(i);
+        });
+    }
+
+    private void getAdmin(){
+        caixa.setOnClickListener((v)->{
+            Intent i = new Intent(MenuActivity.this, AdminActivity.class);
+            startActivity(i);
+        });
+    }
+
+    private void getOferta(){
+        caixa.setOnClickListener((v)->{
+            Intent i = new Intent(MenuActivity.this, OfertaActivity.class);
+            startActivity(i);
+        });
+    }
+
+    private void getCondicoes(){
+        caixa.setOnClickListener((v)->{
+            Intent i = new Intent(MenuActivity.this, RulerActivity.class);
+            startActivity(i);
+        });
+    }
+
 }
