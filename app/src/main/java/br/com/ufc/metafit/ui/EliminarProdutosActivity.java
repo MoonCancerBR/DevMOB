9b55235426395a6b19d22de15ace5e9a2cc3eb1f
MoonCancerBR <fernandogamorim730@gmail.com>
2023-12-01 20:46:25 -0300

    updates

---
+++ app/src/main/java/br/com/ufc/metafit/ui/EliminarProdutosActivity.java
@@ -0,0 +1,16 @@
+package br.com.ufc.metafit.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.ufc.metafit.R;
+
+public class EliminarProdutosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eliminar_produtos);
+    }
+}
---
+++ app/src/main/java/br/com/ufc/metafit/ui/RegistrarProdutosActivity.java
@@ -0,0 +1,16 @@
+package br.com.ufc.metafit.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.ufc.metafit.R;
+
+public class RegistrarProdutosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar_produtos);
+    }
+}
