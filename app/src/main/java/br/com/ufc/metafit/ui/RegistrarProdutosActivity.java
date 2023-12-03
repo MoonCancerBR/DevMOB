391afd88fdd5dae6f40d43372f2eee99f29749a3
MoonCancerBR <fernandogamorim730@gmail.com>
2023-12-02 22:39:41 -0300

    upload camera

--- app/src/main/java/br/com/ufc/metafit/ui/RegistrarProdutosActivity.java
+++ app/src/main/java/br/com/ufc/metafit/ui/RegistrarProdutosActivity.java
@@ -1,13 +1,18 @@
 package br.com.ufc.metafit.ui;
 
+import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -16,14 +21,12 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayOutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-import javax.annotation.Nullable;
-
 import br.com.ufc.metafit.R;
-import br.com.ufc.metafit.databinding.ActivityMainBinding;
 import br.com.ufc.metafit.databinding.ActivityRegistrarProdutosBinding;
 
 public class RegistrarProdutosActivity extends AppCompatActivity {
@@ -32,70 +35,109 @@ public class RegistrarProdutosActivity extends AppCompatActivity {
     Uri imageUri;
     StorageReference storageReference;
     ProgressDialog progressDialog;
+    private StorageReference mStorageRef;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar_produtos);
+        mStorageRef = FirebaseStorage.getInstance().getReference();
         binding = ActivityRegistrarProdutosBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.btnGaleria.setOnClickListener(new View.OnClickListener(){
+        binding.btnGaleria.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 selectImage();
             }
         });
 
-        binding.btnSalvar.setOnClickListener(new View.OnClickListener(){
+        binding.btnCamera.setOnClickListener(new View.OnClickListener() {
             @Override
-           public void onClick(View v){
+            public void onClick(View v) {
+                captureImage();
+            }
+        });
+
+        binding.btnSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 uploadImage();
             }
         });
-
     }
 
-    private void uploadImage(){
-        progressDialog = new ProgressDialog(this);
-        progressDialog.setTitle("Carregando");
-        progressDialog.show();
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss", Locale.CANADA);
-        Date now = new Date ();
-        String filename = formatter.format(now);
-        storageReference = FirebaseStorage.getInstance().getReference("images/"+filename);
-        storageReference.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-            @Override
-            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-
-                binding.firebaseimage.setImageURI(null);
-                Toast.makeText(RegistrarProdutosActivity.this,"Sucesso",Toast.LENGTH_SHORT).show();
-                if(progressDialog.isShowing())
-                    progressDialog.dismiss();
-
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                if(progressDialog.isShowing())
-                    progressDialog.dismiss();
-                Toast.makeText(RegistrarProdutosActivity.this,"Erro",Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
-
-    private void selectImage(){
+    private void selectImage() {
         Intent intent = new Intent();
         intent.setType("image/*");
         intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(intent,100);
+        startActivityForResult(intent, 100);
     }
+
+    private void captureImage() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, 101);
+        }
+    }
+
+    private void onCaptureImageResult(Intent data) {
+        Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+        imageUri = getImageUri(getApplicationContext(), thumbnail);
+        binding.firebaseimage.setImageBitmap(thumbnail);
+    }
+
+    private Uri getImageUri(Context context, Bitmap bitmap) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(context.getContentResolver(), bitmap, "Title", null);
+        return Uri.parse(path);
+    }
+
+    private void uploadImage() {
+        if (imageUri != null) {
+            progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Carregando");
+            progressDialog.show();
+
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss", Locale.CANADA);
+            Date now = new Date();
+            String filename = formatter.format(now);
+            storageReference = FirebaseStorage.getInstance().getReference("images/" + filename);
+            storageReference.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    binding.firebaseimage.setImageURI(null);
+                    Toast.makeText(RegistrarProdutosActivity.this, "Sucesso", Toast.LENGTH_SHORT).show();
+                    if (progressDialog.isShowing())
+                        progressDialog.dismiss();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    if (progressDialog.isShowing())
+                        progressDialog.dismiss();
+                    Toast.makeText(RegistrarProdutosActivity.this, "Erro", Toast.LENGTH_SHORT).show();
+                }
+            });
+        } else {
+            Toast.makeText(this, "Selecione uma imagem antes de salvar.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
-        super.onActivityResult(requestCode, resultCode,data);
-        if(requestCode==100 && data != null & data.getData()!=null);
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-        imageUri = data.getData();
-        binding.firebaseimage.setImageURI(imageUri);
-
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == 100) {
+                if (data != null && data.getData() != null) {
+                    imageUri = data.getData();
+                    binding.firebaseimage.setImageURI(imageUri);
+                }
+            } else if (requestCode == 101) {
+                onCaptureImageResult(data);
+            }
+        }
     }
 }
